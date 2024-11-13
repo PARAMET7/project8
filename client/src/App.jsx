9bfd7f908e65691461c30c203f902d7157e9b9f6
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-11-13 12:36:38 +0100

    done with rendering the products on the dom and create a function of display products

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,76 @@ import DashBoard from './pages/Dashboard';
 axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.withCredentials = true;
 
-const cardBtn = document.qu
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
 
+const cartItems = document.querySelector('.cart-items');
+const cartContent = document.querySelector('.cart-content');
+const cartTotal = document.querySelector('.cart-total');
+const productsDOM = document.querySelector('.products-center');
+
+//cart
+let cart = [];
+//getting products
+class Products{
+    async getProducts() {
+      try {
+        let result = await fetch('../public/products.json');
+        let data = await result.json();
+
+        let products = data.items;
+        products = products.map(item => {
+          const{title, price} = item.fields;
+          const {id} = item.sys;
+          const image = item.fields.image.fields.file.url;
+          return {title, price, id, image}
+        });
+        return products;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+}
+//display the products
+class UI {
+  displayProducts(products){
+    let result = '';
+    products.forEach(product => {
+      result +=`
+        <article class="product">
+          <div class="img-container">
+            <img src=${product.image} alt="product" class="product-img">
+            <button class="bag-btn" data-id="${product.id}">
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+      `;
+    });
+    if (productsDOM) { // Ensure productsDOM is defined
+      productsDOM.innerHTML = result;
+    } else {
+      console.error('productsDOM is not defined or not found in the document.');
+    }
+  }
+}
+
+// class Storage {
+
+// }
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  const ui = new UI()
+  const products = new Products()
+  //get all the products;
+  products.getProducts().then(products => ui.displayProducts(products));
+})
 
 function App() {
   return (
@@ -29,7 +97,5 @@ function App() {
       </UserContextProvider>
   );
 }
-
-
 
 export default App;
